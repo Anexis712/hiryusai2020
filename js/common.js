15769f9b3033590a7b29f6629d8fb8139ac20c02
Ray Otsuka <rayrayo0509@gmail.com>
2021-01-10 20:57:23 +0900

    upload common.js

--- js/common.js
+++ js/common.js
@@ -7,3 +7,29 @@ function auto_scroll(num, num2) {
     }, num2, function() {
     });
 }
+
+
+auth_user_type();
+function auth_user_type (){
+    var code_array = localStorage.token.split(',');
+    let right_8_number = parseInt(code_array[0] + code_array[1] + code_array[2] + code_array[3] + code_array[4] + code_array[5] + code_array[6] + code_array[7]);
+    if(right_8_number % 7 === parseInt(code_array[8]) && right_8_number % 9 === parseInt(code_array[9])){
+        if((parseInt(code_array[0] + code_array[1]) % 5) === 1 || (parseInt(code_array[0] + code_array[1]) % 5) === 0){
+            console.log('生徒 / 教員ログイン');
+            $('.hide_projects').removeClass();
+        }else if((parseInt(code_array[0] + code_array[1]) % 5) === 4){
+            if(code_array[2] === '0'){
+                console.log('生徒 / 教員ログイン');
+                $('.hide_projects').removeClass();
+            }else{
+                //一般
+                console.log('一般ログイン');
+            }    
+        }else{
+            //一般
+            console.log('一般ログイン');
+        }
+    }else{
+        alert('認証エラー。最初からやり直してください。')
+    }
+}
